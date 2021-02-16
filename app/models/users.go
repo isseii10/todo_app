9c8bc2e9856f7ce9e7acb607f7076f9733cdc613
itss10 <t1l.g2.o11g@gmail.com>
2021-02-17 05:32:40 +0900

    User CRUD作成

--- app/models/users.go
+++ app/models/users.go
@@ -11,7 +11,7 @@ type User struct {
 	Name      string
 	Email     string
 	Password  string
-	createdAt time.Time
+	CreatedAt time.Time
 }
 
 func (u *User) CreateUser() (err error) {
@@ -28,6 +28,39 @@ func (u *User) CreateUser() (err error) {
 		u.Email,
 		Encrypt(u.Password),
 		time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func GetUser(id int) (user User, err error) {
+	user = User{}
+	cmd := `select id, uuid, name, email, password, created_at
+	from users where id = ?`
+	err = Db.QueryRow(cmd, id).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+	return user, err
+}
+
+func (u *User) UpdateUser() (err error) {
+	cmd := `update users set name = ?, email = ? where id = ?`
+	_, err = Db.Exec(cmd, u.Name, u.Email, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func (u *User) DeleteUser() (err error) {
+	cmd := `delete from users where id = ?`
+	_, err = Db.Exec(cmd, u.ID)
 	if err != nil {
 		log.Fatalln(err)
 	}
